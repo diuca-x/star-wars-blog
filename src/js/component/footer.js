0c5accda363f9bf2c44b31e4771251d4de975698
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-05-22 19:26:53 +0000

    missing changes

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,13 +2,7 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="py-3 bottom border-top mt-5 bg-dark "  >
-            <ul className="nav justify-content-center  pb-3 mb-0">
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary link-light ">Home</a></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary link-light">Features</a></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary link-light">Pricing</a></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary link-light">FAQs</a></li>
-            <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary link-light">About</a></li>
-            </ul>
-            <p className="text-center text-body-secondary text-light">© 2023 Company, Inc</p>
+            
+            <p className="text-center text-body-secondary text-light">© 2023 Star Wars Inc</p>
     </footer>
 );
